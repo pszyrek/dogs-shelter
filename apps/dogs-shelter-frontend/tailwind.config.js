6eb900c7e5426954344e3719e139f54a3fac4d57
Piotr Bujna <piotr.bujna@bold.com>
2025-11-24 21:36:55 +0100

    feat: Update repo configuration

--- apps/dogs-shelter-frontend/tailwind.config.js
+++ apps/dogs-shelter-frontend/tailwind.config.js
@@ -1,12 +1,16 @@
-import baseConfig from "../../packages/ui/tailwind.config";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      ...baseConfig.theme.extend,
+      colors: {
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+      },
+      spacing: {
+        128: "32rem",
+      },
     },
   },
-  plugins: [baseConfig.plugins],
+  plugins: [],
 };
