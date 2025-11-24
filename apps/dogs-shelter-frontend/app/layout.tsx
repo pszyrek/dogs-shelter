eec81a0862087a5a9133d9f8464aa09a514fd043
Piotr Bujna <piotr.bujna@bold.com>
2025-11-24 20:18:26 +0100

    feat: Remove submodule from frontend app

---
+++ apps/dogs-shelter-frontend/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Dogs Shelter Frontend",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
